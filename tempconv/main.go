f565e03643658686f05c41627eb6fdcb47f9c6a4
Guilherme Teixeira Ais <guilhermeteixeiraais@gmail.com>
2024-12-06 11:41:29 -0300

    feat: implement main temperature conversion application

---
+++ tempconv/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	tempconv "tempconv/lib"
+)
+
+func main() {
+
+	for _, arg := range os.Args[1:] {
+		temperatureHandler(arg)
+	}
+
+	input := bufio.NewScanner(os.Stdin)
+	for input.Scan() {
+		temperatureHandler(input.Text())
+	}
+}
+
+func temperatureHandler(arg string) {
+	t, err := strconv.ParseFloat(arg, 64)
+	if err != nil {
+		fmt.Fprintf(os.Stdout, "error converting the arg %v: %v\n", arg, err)
+	}
+
+	f := tempconv.Fahrenheit(t)
+	c := tempconv.Celsius(t)
+
+	fmt.Printf("%s = %s, %s = %s\n", f, tempconv.FToC(f), c, tempconv.CToF(c))
+}
